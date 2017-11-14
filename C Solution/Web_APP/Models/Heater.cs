195341e1a1ce223eaddff2c654bba7178f43b0b0
Thijs Bouwes <thijsbouwes@gmail.com>
2017-11-14 18:55:03 +0000

    Added the heater to the api

---
+++ C Solution/Web_APP/Models/Heater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_API.Models
+{
+    public class Heater
+    {
+        public int id { get; set; }
+        public int temperature { get; set; }
+    }
+}
