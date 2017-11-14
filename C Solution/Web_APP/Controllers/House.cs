3f5083a7316af48cb3824df2d3e4dc01bad2ca39
Thijs Bouwes <thijsbouwes@gmail.com>
2017-11-14 17:44:40 +0000

    Added a endpoint for the lamps

---
+++ C Solution/Web_APP/Controllers/House.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Sockets;
+
+namespace Web_API.Controllers
+{
+    public class House
+    {
+        public static string server_address = "127.0.0.1";
+        public int server_port = 11000;
+        public TcpClient client;
+        public NetworkStream stream;
+
+        public House() {
+            // Create a TcpClient.
+            client = new TcpClient(server_address, server_port);
+
+            // Get a client stream for reading and writing.
+            stream = client.GetStream();
+        }
+
+        public string GetState(String message)
+        {
+            message += Environment.NewLine;
+
+            Send(message);
+
+            return Read().Trim();
+        }
+
+        public string Read()
+        {
+            // Buffer to store the response bytes.
+            byte[] dataBuffer = new byte[client.ReceiveBufferSize];
+
+            // Read the first batch of the TcpServer response bytes.
+            int BytesRead = stream.Read(dataBuffer, 0, dataBuffer.Length);
+
+            // Recived bytes to string
+            return System.Text.Encoding.ASCII.GetString(dataBuffer, 0, BytesRead);
+        }
+
+
+        public void Send(string message)
+        {
+            // Translate the passed message into ASCII and store it as a Byte array.
+            byte[] dataBuffer = System.Text.Encoding.ASCII.GetBytes(message);
+
+            // Send the message to the connected TcpServer. 
+            stream.Write(dataBuffer, 0, dataBuffer.Length);
+        }
+
+        public void Close()
+        {
+            // Close everything.
+            byte[] dataBuffer = System.Text.Encoding.ASCII.GetBytes("exit" + Environment.NewLine);
+            stream.Write(dataBuffer, 0, dataBuffer.Length);
+
+            // Buffer to store the response bytes.
+            dataBuffer = new byte[client.ReceiveBufferSize];
+            stream.Read(dataBuffer, 0, dataBuffer.Length);
+
+            client.Close();
+            stream.Close();
+        }
+    }
+}
---
+++ C Solution/Web_APP/Controllers/LampController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web_API.Models;
+
+namespace Web_API.Controllers
+{
+    public class LampController : ApiController
+    {
+
+        public IHttpActionResult Get()
+        {
+            House House = new House();
+
+            // Ask for lamps
+            string numberOfLamps = House.GetState("lamps");
+
+            // Create array for lamps
+            Lamp[] Lamps = new Lamp[int.Parse(numberOfLamps)];
+
+            // Ask status foreach lamp
+            for(int index = 0; index < Lamps.Length; index++)
+            {
+                string OnOff = House.GetState("lamp " + index.ToString());
+                string Description = House.GetState("whereis lamp " + index);
+
+                int Status = OnOff.Contains("Off") ? 0 : 1;
+
+                Lamps[index] = new Lamp { id = index, status = Status, description = Description };
+            }
+
+            House.Close();
+            return Json(Lamps);
+        }
+    }
+}
