5c81e456d28f0cc5f3109dc52a50776a9ffe60b5
Jeroen Groenveld <jeroengroenveld@live.nl>
2017-11-21 22:14:21 +0100

    publish-project.sh variables to the Server Environment

--- publish-project.sh
+++ publish-project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-project="C:\GIT\InHolland\robotica-domotica-webdashboard\C# Solution - Good\C# Solution.sln"
-publish_dir="C:\Users\Jeroen\Desktop\test"
+project="./C# Solution - Good/C# Solution.sln"
+publish_dir="C:\inetpub\wwwroot\domotica.j20.nl"
 dotnet publish "$project" --output "$publish_dir"
 echo "Project has been published."
