5f8d3149edf4addbe02893bbf50743d915386e37
Jeroen Groenveld <jeroengroenveld@live.nl>
2017-11-22 17:02:30 +0100

    updated publish script.

--- publish-project.sh
+++ publish-project.sh
@@ -2,5 +2,14 @@
 
 project="./C# Solution - Good/C# Solution.sln"
 publish_dir="C:\inetpub\wwwroot\domotica.j20.nl"
+
+#stop the website in iis
+appcmd stop site domotica.j20.nl
+
+#publish project
 dotnet publish "$project" --output "$publish_dir"
+
+#start the website in iis
+appcmd start site domotica.j20.nl
+
 echo "Project has been published."
