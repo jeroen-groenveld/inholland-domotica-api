f201a5d4b4a50e3443c7a67b43b168878a8fed87
Jeroen Groenveld <jeroengroenveld@live.nl>
2017-11-24 13:12:45 +0100

    Updated publish.sh

--- publish-project.sh
+++ publish-project.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-project="./C# Solution - Good/C# Solution.sln"
-publish_dir="C:\inetpub\wwwroot\domotica.j20.nl"
+project="./ASP.NET Solution/Web API/Web_API.csproj"
+publish_dir="C:\inetpub\wwwroot\api.domotica.j20.nl"
 
 #stop the website in iis
-appcmd stop site domotica.j20.nl
+appcmd stop site api.domotica.j20.nl
 
 #publish project
 dotnet publish "$project" --output "$publish_dir"
 
 #start the website in iis
-appcmd start site domotica.j20.nl
+appcmd start site api.domotica.j20.nl
 
 echo "Project has been published."
