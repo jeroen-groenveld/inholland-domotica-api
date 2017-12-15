6e4d47222cf47df764602fa7983d6eb64ce79f61
Jeroen Groenveld <jeroengroenveld@live.nl>
2017-12-15 11:39:20 +0100

    Added units param to weather endpoint.

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -11,38 +11,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Domotica_API.Controllers
 {
-    [Route(Config.App.API_ROOT_PATH + "/weather")]
-    [MiddlewareFilter(typeof(TokenAuthorize))]
-    public class WeatherController : ApiController
-    {
-        //Constructor
-        public WeatherController(DatabaseContext db) : base(db) { }
+	[Route(Config.App.API_ROOT_PATH + "/weather")]
+	[MiddlewareFilter(typeof(TokenAuthorize))]
+	public class WeatherController : ApiController
+	{
+		//Constructor
+		public WeatherController(DatabaseContext db) : base(db) { }
 
-        [HttpGet("lat/{latitude}/lon/{longitude}/cnt/{days}/lang/{lang}")]
-        public IActionResult GetWeather(string latitude, string longitude, int days, string language)
-        {
-            string appid = env.WEATHER_API_KEY;
-            string body = string.Empty;
-            string url = Config.Weather.ENDPOINT + String.Format($"?lat={latitude}&lon={longitude}&cnt={days}&lang={language}&appid={appid}");
+		[HttpGet("lat/{latitude}/lon/{longitude}/cnt/{days}/lang/{lang}/units/{units}")]
+		public IActionResult GetWeather(string latitude, string longitude, int days, string language, string units)
+		{
+			string appid = env.WEATHER_API_KEY;
+			string body = string.Empty;
 
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                request.AutomaticDecompression = DecompressionMethods.GZip;
+			string url = Config.Weather.ENDPOINT + String.Format($"?lat={latitude}&lon={longitude}&cnt={days}&lang={language}&units={units}&appid={appid}");
 
-                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-                using (Stream stream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    body = reader.ReadToEnd();
-                }
+			try
+			{
+				HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+				request.AutomaticDecompression = DecompressionMethods.GZip;
 
-                return Ok(body);
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.ToString());
-            }
-        }
-    }
+				using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+				using (Stream stream = response.GetResponseStream())
+				using (StreamReader reader = new StreamReader(stream))
+				{
+					body = reader.ReadToEnd();
+				}
+				return Ok(Newtonsoft.Json.JsonConvert.DeserializeObject(body));
+			}
+			catch (Exception e)
+			{
+				return BadRequest(e.ToString());
+			}
+		}
+	}
 }
